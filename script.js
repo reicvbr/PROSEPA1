d63e926cdbc4328bb808bfa3074c22beed273f8a
reicvbr <REIUSAPINA@GMAIL.COM>
2024-10-31 03:46:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Adicione qualquer script JavaScript que você precisar aqui
+document.getElementById('contatoForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulário enviado!');
+});
